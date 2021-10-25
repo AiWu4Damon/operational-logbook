f2859141b615d9914b487608a269156665508e5b
Damon <dameng.fan@tuya.com>
2021-10-25 20:05:33 +0800

    1.init

---
+++ logbook-service/src/main/java/com/damon/logbook/LogRecord.java
@@ -0,0 +1,38 @@
+package com.damon.logbook;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 操作日志的核心注解
+ * @author damon
+ * @version 1.0
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+public @interface LogRecord {
+
+    /**
+     *
+     * @return 业务ID
+     */
+    String bizNo();
+
+    /**
+     *
+     * @return 操作人
+     */
+    String operator();
+
+    /**
+     *
+     * @return 模版
+     */
+    String content();
+}
---
+++ logbook-service/src/main/java/com/damon/logbook/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author 达蒙
+ * @date 2021/10/25 7:26 下午
+ * @version 1.0
+ */
+package com.damon.logbook;
